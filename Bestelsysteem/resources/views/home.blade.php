2e4fba5da45067b490e3af4e8ccf6b578d98bd7c
sintcode <dorian@adiworks.nl>
2024-04-17 12:11:38 +0200

    Start Of Realising Mockups

--- Bestelsysteem/resources/views/home.blade.php
+++ Bestelsysteem/resources/views/home.blade.php
@@ -1,23 +1,57 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="d-flex justify-content-evenly">
+    <div name="knoppenlijsten" class="col-3 col-sm-4">
+        <div name="bestelknoppen" class="rounded p-3 bg-gradient bg-primary bg-opacity-25">
+            <h1 class="text-center font-semibold mb-2"> Bestellen </h1>
+            <ul class="list-unstyled p-2 text-center">
+                <li class="shadow shadow-bm rounded text-white border border-white border-opacity-50 font-bold p-1 bg-primary bg-opacity-75 mb-2"><h4 class="my-1"> Nieuwe Bestelling </h4></li>
+                <li class="shadow shadow-bm rounded text-white border border-white border-opacity-50 font-bold p-1 bg-primary bg-opacity-75 my-2"><h4 class="my-1"> Bestelling Herhalen</h4></li>
+                <li class="shadow shadow-bm rounded text-white border border-white border-opacity-50 font-bold p-1 bg-primary bg-opacity-75 my-2"><h4 class="my-1"> Bestelling Hervatten </h4></li>
+            </ul>
         </div>
+        <div name="adminknoppen" class="rounded p-3 bg-gradient bg-primary bg-opacity-25 mt-5">
+            <h1 class="text-center font-semibold mb-2"> Administrator </h1>
+            <ul class="list-unstyled p-2 text-center">
+                <li class="shadow shadow-bm rounded text-white border border-white border-opacity-50 font-bold p-1 bg-primary bg-opacity-75 mb-2"><h4 class="my-1"> Databasebeheer </h4></li>
+                <li class="shadow shadow-bm rounded text-white border border-white border-opacity-50 font-bold p-1 bg-primary bg-opacity-75 my-2"><h4 class="my-1"> Gebruikersbeheer </h4></li>
+            </ul>
+        </div>
+    </div>
+    <div name="bestellingsoverzicht" class="col-6 col-sm-6 rounded p-3 bg-gradient bg-primary bg-opacity-25">
+        <h1 class="text-center mb-0 font-semibold mb-2"> Bestellingen </h1>
+        <ul class="bg-white bg-gradient shadow shadow-sm border border-opacity-25 rounded list-unstyled p-2">
+            <li class="mx-auto col-12 col-sm-10 rounded font-semibold d-flex justify-content-evenly">
+                <h4>Datum</h4>
+                <h4>Bestelling</h4>
+                <h4>Status</h4>
+            </li>
+            <li class="mx-auto col-12 col-sm-10 rounded bg-white border border-black shadow shadow-sm bg-gradient font-semibold d-flex justify-content-evenly mb-2">
+                <p class="mt-0 mb-0">12/12/2022</p>
+                <p class="mt-0 mb-0">1234567890</p>
+                <p class="mt-0 mb-0">Voltooid</p>
+            </li>
+            <li class="mx-auto col-12 col-sm-10 rounded bg-white border border-black shadow shadow-sm bg-gradient font-semibold d-flex justify-content-evenly my-2">
+                <p class="mt-0 mb-0">26/12/2022</p>
+                <p class="mt-0 mb-0">1345678901</p>
+                <p class="mt-0 mb-0">Voltooid</p>
+            </li>
+            <li class="mx-auto col-12 col-sm-10 rounded bg-white border border-black shadow shadow-sm bg-gradient font-semibold d-flex justify-content-evenly my-2">
+                <p class="mt-0 mb-0">2/1/2023</p>
+                <p class="mt-0 mb-0">1456789012</p>
+                <p class="mt-0 mb-0">Voltooid</p>
+            </li>
+            <li class="mx-auto col-12 col-sm-10 rounded bg-white border border-black shadow shadow-sm bg-gradient font-semibold d-flex justify-content-evenly my-2">
+                <p class="mt-0 mb-0">13/1/2023</p>
+                <p class="mt-0 mb-0">1567890123</p>
+                <p class="mt-0 mb-0">Voltooid</p>
+            </li>
+            <li class="mx-auto col-12 col-sm-10 rounded bg-white border border-black shadow shadow-sm bg-gradient font-semibold d-flex justify-content-evenly my-2">
+                <p class="mt-0 mb-0">24/1/2023</p>
+                <p class="mt-0 mb-0">1678901234</p>
+                <p class="mt-0 mb-0">Voltooid</p>
+            </li>
+        </ul>
     </div>
 </div>
 @endsection
--- Bestelsysteem/resources/views/layouts/app.blade.php
+++ Bestelsysteem/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Bestelsysteem') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -18,49 +18,38 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+        <nav class="navbar navbar-expand-md navbar-light bg-white bg-gradient bg-opacity-25 shadow-sm">
+            <div class="">
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Bestelsysteem') }}</a> --}}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <div class="d-flex">
+                        <img class="h-25 w-25 border border-opacity-75 shadow-sm rounded" src="https://cdn.discordapp.com/attachments/1204338336047697980/1230096062707007488/cow.png?ex=6632133b&is=661f9e3b&hm=08470be27fd0d1fefdcfff25ec492a510c009c27a6a5d8da6022bdcbd92f68d4&">
+                        <h4 class="ms-3 my-auto">Rik Bakker</h4>
+                    </div>
 
-                    </ul>
+                    <!-- Middle Side Of Navar -->
+                    <img class="mx-auto w-25" href="{{ url('/') }}" src="https://cdn.discordapp.com/attachments/1204338336047697980/1230095705927061594/download.png?ex=663212e6&is=661f9de6&hm=8d33b1638a2e539c47a06540757333c01e895458e391fa5e1cc3f1df09c24869&">
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                <li class="nav-item"> <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <li class="nav-item"> <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }}</a>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -71,7 +60,6 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
