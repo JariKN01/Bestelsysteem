15b5e406f2fa6f809a691a78179530b43f0c7fa6
Kim Nguyen <144351958+kbngn@users.noreply.github.com>
2024-05-28 13:18:37 +0200

    New pages

--- Bestelsysteem/routes/web.php
+++ Bestelsysteem/routes/web.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BestelformulierController;
 use App\Http\Controllers\DatabasebeheerController;
 use App\Http\Controllers\GebruikersbeheerController;
+use App\Http\Controllers\AfdelingController;
 
 Route::get('/', function () {return view('home');})->name('home');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/bestelformulier', [BestelformulierController::class, 'create']);
+Route::get('/home/bestelformulier', function () {return view('bestelformulier');})->name('bestelformulier');
 Route::get('/databasebeheer', [DatabasebeheerController::class, 'index'])->name('databasebeheer');
 Route::get('/gebruikersbeheer', [GebruikersbeheerController::class, 'index'])->name('gebruikersbeheer');
-Route::get('/home/bestelformulier', function () {return view('bestelformulier');})->name('bestelformulier');
-
+Route::get('/afdeling', [AfdelingController::class, 'index'])->name('afdeling');
+Route::get('/databasebeheer/afdeling', function () {return view('afdeling');})->name('afdeling');
