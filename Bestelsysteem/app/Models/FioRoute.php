f65cda4f5f8f9db2fa2945889088fb7f06e67d12
sintcode <dorian@adiworks.nl>
2024-05-30 18:52:45 +0200

    Models/Migrations

---
+++ Bestelsysteem/app/Models/FioRoute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FioRoute extends Model
+{
+    use HasFactory;
+}
