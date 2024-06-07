5a3532dd8205b9306726c16b13eecfd0f7083da0
JariKN01 <jariknoop2@gmail.com>
2024-06-07 15:22:52 +0200

    Adding default admin account

---
+++ Bestelsysteem/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'email'    => 'admin@almere.nl',
+            'role' => 'admin',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $user->roles()->attach(1);
+    }
+}
